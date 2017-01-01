3e33a5d4f1702c46df8c34f831c234900643508c
CorporalDead <jedeyenyt654@gmail.com>
2017-01-01 21:30:50 +0800

    added variables for enemy class

--- PhysicsTest/Enemy.cs
+++ PhysicsTest/Enemy.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +11,24 @@ namespace PhysicsTest
 {
     class Enemy
     {
+        private Rectangle _enemyRect;
+        private Texture2D _playerTexture;
 
+        //Animation variables
+        private int _SpriteSheetX;
+        private int _SpriteSheetY;
+
+        private int _animationDelay;
+
+        private bool movingLeft;
+        private bool movingRight;
+        private bool idleRight;
+        private bool idleLeft;
+        //end animation vars
+
+        public Enemy(Rectangle _enemyRect, Texture2D _enemyTexture)
+        {
+
+        }
     }
 }
