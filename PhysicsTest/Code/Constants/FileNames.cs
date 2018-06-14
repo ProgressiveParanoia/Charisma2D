fd71686692b5f2ca19485c7c7298590118ba8411
progressiveparanoia <jedeyenyt654@gmail.com>
2018-06-14 23:36:31 +0800

    constant for file names

---
+++ PhysicsTest/Code/Constants/FileNames.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhysicsTest.Code.Constants
+{
+    public static class FileNames
+    {
+        public const string Platform_CheckPoint = "Platform_Checkpoint";
+        public const string Platform_Exit = "Platform_Exit";
+        public const string Platform_Regular = "Platform_Regular";
+        public const string Platform_Slippery = "Platform_Slippery";
+        public const string Platform_SpawnPoint = "Platform_SpawnPoint";
+        public const string Platform_Spikes = "Platform_Spikes";
+        public const string Platform_Tall_Plat = "Platform_Tall_Plat";
+
+    }
+}
