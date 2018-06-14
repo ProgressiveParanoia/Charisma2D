2a3ff5afb9bbb677bb2c49abf45d91f1bebe1fa4
progressiveparanoia <jedeyenyt654@gmail.com>
2018-06-14 23:36:18 +0800

    base class for game objects

---
+++ PhysicsTest/Code/Game_Objects/GameObject.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ParanoidGames.Code
+{
+    class GameObject
+    {
+        internal Rectangle rect;
+        internal Texture2D texture;
+        internal Color color;
+       
+        public Rectangle Rectangle
+        {
+            get
+            {
+                return rect;
+            }
+        }
+
+        public Texture2D Texture
+        {
+            get
+            {
+                return texture;
+            }
+        }
+
+        public Color Color
+        {
+            get
+            {
+                return color;
+            }
+        }
+       
+        public Point Position
+        {
+            get
+            {
+                return rect.Location;
+            }
+
+            internal set
+            {
+                rect.Location = value;
+            }
+        }
+    }
+}
