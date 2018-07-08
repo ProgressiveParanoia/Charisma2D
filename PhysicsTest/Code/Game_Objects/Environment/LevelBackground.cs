a97cdc9b87b60b9a294c26942f28558775fa0284
progressiveparanoia <jedeyenyt654@gmail.com>
2018-07-08 19:05:33 +0800

    placed background image in proper file folder

---
+++ PhysicsTest/Code/Game_Objects/Environment/LevelBackground.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ParanoidGames.Charisma2D
+{
+    class LevelBackground : GameObject
+    {
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            this.textureName = "GameBackground";
+        }
+    }
+}
